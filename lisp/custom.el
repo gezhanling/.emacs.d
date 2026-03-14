d8c953e8ec33a4a40dac6b1ec6bf773d58de83e3
gezhanling <1138440880@qq.com>
2026-03-14 11:35:10 +0800

    custom.el change

--- lisp/custom.el
+++ lisp/custom.el
@@ -5,11 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
  '(markdown-command "pandoc --from=markdown --to=html5")
- '(package-selected-packages
-	 '(doom-modeline doom-themes embark-consult evil-anzu evil-collection
-									 evil-nerd-commenter evil-snipe evil-surround
-									 orderless org-roam-ui org-superstar treesit-auto
-									 undo-tree vertico wgrep))
+ '(package-selected-packages nil)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
